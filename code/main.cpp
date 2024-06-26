343f121fc667a661a643578a0b34fd6b57baae97
Sam Kuttenkuler <33381106+skuttenkuler@users.noreply.github.com>
2024-06-26 11:42:14 -0500

    COLORS ON THE SCREEN WITH BITMAP

--- code/main.cpp
+++ code/main.cpp
@@ -1,50 +1,74 @@
 #include <windows.h>
+#include <stdint.h>
 
 #define internal static
 #define local_persist static
 #define global_variable static
 
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32; 
+typedef uint64_t uint64;
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
 global_variable bool running;
 global_variable BITMAPINFO BitmapInfo;
 global_variable void *BitmapMemory;
-global_variable HBITMAP BitmapHandle;
-global_variable HDC BitmapDeviceContext;
+global_variable int BitmapWidth;
+global_variable int BitmapHeight;
+global_variable int BytesPerPixel = 4;
 
-internal void Win32ResizeDIBSection(int Height, int Width)
+internal void Win32ResizeDIBSection(int Width, int Height)
 {
-  if(BitmapHandle)
-  {
-    DeleteObject(BitmapHandle);
-  }
-  if(!BitmapDeviceContext)
-  {
-    BitmapDeviceContext = CreateCompatibleDC(0);
-  }
 
-  BITMAPINFO BitmapInfo;
+  if(BitmapMemory)
+  {
+    VirtualFree(BitmapMemory,0,MEM_RELEASE);
+  }
+  BitmapWidth = Width;
+  BitmapHeight = Height;
+
   BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
-  BitmapInfo.bmiHeader.biWidth = Width;
-  BitmapInfo.bmiHeader.biHeight = Height;
+  BitmapInfo.bmiHeader.biWidth = BitmapWidth;
+  BitmapInfo.bmiHeader.biHeight = -BitmapHeight;//top down
   BitmapInfo.bmiHeader.biPlanes = 1;
   BitmapInfo.bmiHeader.biBitCount = 32;
   BitmapInfo.bmiHeader.biCompression = BI_RGB;
- 
-  //todo future memory management
-
-  BitmapHandle = CreateDIBSection(
-    BitmapDeviceContext, 
-    &BitmapInfo,
-    DIB_RGB_COLORS,
-    &BitmapMemory,
-    0,0);
+  
+  int BitmapMemorySize = (Width*Height)*BytesPerPixel;
+  BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+  
+  uint8 *Row = (uint8 *)BitmapMemory;
+  int Pitch = Width*BytesPerPixel;
+  
+  for(int Y = 0; Y < BitmapHeight; ++Y){
+    uint8 *Pixel = (uint8 *)Row;
+    for(int X = 0; X < BitmapWidth; ++X){
+      *Pixel = 255;
+      ++Pixel;  
+      *Pixel = 0;
+      ++Pixel;  
+      *Pixel = 0;
+      ++Pixel;  
+      *Pixel = 0;
+      ++Pixel;  
+    }
+    Row += Pitch;
+  }
 }
 
-internal void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Height, int Width)
+internal void Win32UpdateWindow(HDC DeviceContext, RECT *WindowRect, int X, int Y, int Width, int Height)
 {
+  int WindowWidth = WindowRect->right - WindowRect->left;
+  int WindowHeight = WindowRect->bottom - WindowRect->top;
   StretchDIBits(
-      BitmapDeviceContext,
-      X, Y, Width, Height, 
-      X, Y, Width, Height,
+      DeviceContext,
+      0, 0, BitmapWidth, BitmapHeight, 
+      0, 0, WindowWidth, WindowHeight, 
       BitmapMemory,
       &BitmapInfo,
       DIB_RGB_COLORS,
@@ -54,15 +78,15 @@ internal void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Height, int
 
 LRESULT CALLBACK Win32MainWindowCallback(HWND Window, UINT Message, WPARAM wParam, LPARAM lParam)
 {
-LRESULT Result = 0;
+  LRESULT Result = 0;
 
 switch (Message) {
   case WM_SIZE:
     {
       RECT ClientRect;
       GetClientRect(Window,&ClientRect);
-      int Height = ClientRect.top - ClientRect.bottom;
-      int Width = ClientRect.left - ClientRect.right;
+      int Width = ClientRect.right - ClientRect.left;
+      int Height = ClientRect.bottom - ClientRect.top;
       Win32ResizeDIBSection(Width,Height);
     }
     break;
@@ -91,20 +115,18 @@ switch (Message) {
       HDC DeviceContext = BeginPaint( Window, &Paint);
       int X = Paint.rcPaint.left;
       int Y = Paint.rcPaint.top;
-      int Height = Paint.rcPaint.top - Paint.rcPaint.bottom;
-      int Width = Paint.rcPaint.left - Paint.rcPaint.right;
-      Win32UpdateWindow(DeviceContext, X, Y, Height, Width);
+      int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+      int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+      RECT ClientRect;
+      GetClientRect(Window, &ClientRect);
+      Win32UpdateWindow(DeviceContext, &ClientRect, X, Y, Width, Height);
       EndPaint(Window, &Paint);
     }
     break;
-  //case END_PAINT:
-   //// {
 
-    //}
-    //bGdi32.libreak;
     default:
     {
-      //OutputDebugStringA("default\n")
+      //OutputDebugStringA("default\n");
       Result = DefWindowProc(Window, Message, wParam,lParam);
     }
     break;
