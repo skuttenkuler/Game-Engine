c46516239cebb70518dd2941c6a68828eb42fea5
Sam Kuttenkuler <33381106+skuttenkuler@users.noreply.github.com>
2024-06-26 09:09:04 -0500

    ready to paint to screen, compiling

--- code/main.cpp
+++ code/main.cpp
@@ -1,32 +1,81 @@
 #include <windows.h>
 
+#define internal static
+#define local_persist static
+#define global_variable static
 
-LRESULT CALLBACK 
-MainWindowCallback(HWND Window,
-              UINT Message,
-              WPARAM wParam,
-              LPARAM lParam)
+global_variable bool running;
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
+
+internal void Win32ResizeDIBSection(int Height, int Width)
+{
+  if(BitmapHandle)
+  {
+    DeleteObject(BitmapHandle);
+  }
+  if(!BitmapDeviceContext)
+  {
+    BitmapDeviceContext = CreateCompatibleDC(0);
+  }
+
+  BITMAPINFO BitmapInfo;
+  BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+  BitmapInfo.bmiHeader.biWidth = Width;
+  BitmapInfo.bmiHeader.biHeight = Height;
+  BitmapInfo.bmiHeader.biPlanes = 1;
+  BitmapInfo.bmiHeader.biBitCount = 32;
+  BitmapInfo.bmiHeader.biCompression = BI_RGB;
+ 
+  //todo future memory management
+
+  BitmapHandle = CreateDIBSection(
+    BitmapDeviceContext, 
+    &BitmapInfo,
+    DIB_RGB_COLORS,
+    &BitmapMemory,
+    0,0);
+}
+
+internal void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Height, int Width)
+{
+  StretchDIBits(
+      BitmapDeviceContext,
+      X, Y, Width, Height, 
+      X, Y, Width, Height,
+      BitmapMemory,
+      &BitmapInfo,
+      DIB_RGB_COLORS,
+      SRCCOPY
+      );
+}
+
+LRESULT CALLBACK Win32MainWindowCallback(HWND Window, UINT Message, WPARAM wParam, LPARAM lParam)
 {
 LRESULT Result = 0;
 
 switch (Message) {
   case WM_SIZE:
     {
-      OutputDebugStringA("WM_SIZE\n");
+      RECT ClientRect;
+      GetClientRect(Window,&ClientRect);
+      int Height = ClientRect.top - ClientRect.bottom;
+      int Width = ClientRect.left - ClientRect.right;
+      Win32ResizeDIBSection(Width,Height);
     }
     break;
 
   case WM_DESTROY:
     {
-
-      OutputDebugStringA("WM_DESTROY\n");
+      running = false;
     }
     break;
 
   case WM_CLOSE:
     {
-      PostQuitMessage(0);
-      OutputDebugStringA("WM_CLOSE\n");
+      running = false;
     }
     break;
 
@@ -44,7 +93,7 @@ switch (Message) {
       int Y = Paint.rcPaint.top;
       int Height = Paint.rcPaint.top - Paint.rcPaint.bottom;
       int Width = Paint.rcPaint.left - Paint.rcPaint.right;
-      PatBlt(DeviceContext,X,Y,Width,Height,WHITENESS);
+      Win32UpdateWindow(DeviceContext, X, Y, Height, Width);
       EndPaint(Window, &Paint);
     }
     break;
@@ -72,7 +121,7 @@ WinMain(HINSTANCE Instance,
   WNDCLASS WindowClass ={};
 
   WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
-  WindowClass.lpfnWndProc = MainWindowCallback;
+  WindowClass.lpfnWndProc = Win32MainWindowCallback;
   WindowClass.hInstance = Instance;
   //WindowClass.hIcon;
   WindowClass.lpszClassName = "KiddEngineWindowClass";
@@ -96,7 +145,8 @@ if(RegisterClass(&WindowClass))
       );
     if(WindowHandle)
     {
-      for(;;)
+      running = true;
+      while(running)
       {
         MSG Message;
         BOOL messageResult = GetMessage(&Message,0,0,0);
